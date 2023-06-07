36c831d86941287944bf52991640ab5724b68416
Crowdbotics <team@crowdbotics.com>
2023-06-07 17:32:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-limit-41339",
+  url: "https://lingering-limit-41339.botics.co",
+  api: "https://lingering-limit-41339.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
